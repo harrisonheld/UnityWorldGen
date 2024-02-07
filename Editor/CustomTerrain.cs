5ab7d878311c2291196f3c68a6b9c4c7b43e57a7
Harrison Held <harrydheld@gmail.com>
2024-02-07 10:45:33 -0600

    Editor GUI button

--- Editor/CustomTerrain.cs
+++ Editor/CustomTerrain.cs
@@ -1,18 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class CustomTerrain : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // Your existing code...
 
-    // Update is called once per frame
-    void Update()
+#if UNITY_EDITOR
+    [CustomEditor(typeof(CustomTerrain))]
+    public class CustomTerrainEditor : Editor
     {
-        
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            CustomTerrain terrain = (CustomTerrain)target;
+
+            GUILayout.Space(10);
+
+            // Create a button in the Inspector
+            if (GUILayout.Button("Generate Terrain"))
+            {
+                // Call the function you want to execute in Editor mode
+                terrain.GenerateTerrain();
+            }
+        }
+    }
+#endif
+
+    // Function to be called when the button is clicked in Editor mode
+    public void GenerateTerrain()
+    {
+        // Implement your terrain generation logic here
+        Debug.Log("Generating Terrain...");
     }
 }
