4ca074758dceeda584b517965d5e02cfa35b566d
Harrison Held <harrydheld@gmail.com>
2024-02-08 15:51:14 -0600

    Mesh is generating.

--- Editor/CustomTerrain.cs
+++ Editor/CustomTerrain.cs
@@ -8,6 +8,11 @@ using UnityEngine;
 public class CustomTerrain : MonoBehaviour
 {
     [SerializeField]
+    private int _width = 50;
+    private int _height = 50;
+    private int _resolution = 250;
+
+    [SerializeField]
     private Biome[] _biomes;
 
 #if UNITY_EDITOR
@@ -30,11 +35,83 @@ public class CustomTerrain : MonoBehaviour
     }
 #endif
 
-    // Function to be called when the button is clicked in Editor mode
     public void GenerateTerrain()
     {
-        // Implement your terrain generation logic here
+        if(_biomes.Length == 0)
+        {
+            Debug.LogError("Cannot generate terrain because no biomes have been added to the terrain.");
+            return;
+        }
+
         Debug.Log("Generating Terrain...");
+        Biome biome = _biomes[0];
+
+        Mesh mesh = new Mesh();
+        mesh.name = "TerrainMesh";
+
+        // Vertices
+        Vector3[] vertices = new Vector3[_resolution * _resolution];
+        for (int x = 0; x < _resolution; x++)
+        {
+            for (int z = 0; z < _resolution; z++)
+            {
+                float normalizedX = x / (float)(_resolution - 1); // Normalize x between 0 and 1
+                float normalizedZ = z / (float)(_resolution - 1); // Normalize z between 0 and 1
+                float worldX = normalizedX * _width - _width / 2;
+                float worldZ = normalizedZ * _height - _height / 2;
+                float height = biome.GetHeightmap().GetHeight(worldX, worldZ);
+                vertices[x * _resolution + z] = new Vector3(worldX, height, worldZ);
+            }
+        }
+        mesh.vertices = vertices;
+
+        // Triangles
+        int[] triangles = new int[(_resolution - 1) * (_resolution - 1) * 6];
+        int triangleIndex = 0;
+        for (int x = 0; x < _resolution - 1; x++)
+        {
+            for (int z = 0; z < _resolution - 1; z++)
+            {
+                int vertexIndex = x * _resolution + z;
+                triangles[triangleIndex] = vertexIndex;
+                triangles[triangleIndex + 1] = vertexIndex + 1;
+                triangles[triangleIndex + 2] = vertexIndex + _resolution;
+                triangles[triangleIndex + 3] = vertexIndex + 1;
+                triangles[triangleIndex + 4] = vertexIndex + _resolution + 1;
+                triangles[triangleIndex + 5] = vertexIndex + _resolution;
+
+                triangleIndex += 6;
+            }
+        }
+        mesh.triangles = triangles;
+
+        // Normals
+        mesh.RecalculateNormals();
+
+        // UVs - for textures
+        Vector2[] uvs = new Vector2[vertices.Length];
+        for (int i = 0; i < uvs.Length; i++)
+        {
+            uvs[i] = new Vector2(vertices[i].x / _resolution, vertices[i].z / _resolution);
+        }
+        mesh.uv = uvs;
+
+        // add all components necessary for rendering a mesh
+        if(GetComponent<MeshFilter>() == null)
+        {
+            this.gameObject.AddComponent<MeshFilter>();
+        }
+        if (GetComponent<MeshRenderer>() == null)
+        {
+            this.gameObject.AddComponent<MeshRenderer>();
+        }
+        if (GetComponent<MeshCollider>() == null)
+        {
+            this.gameObject.AddComponent<MeshCollider>();
+        }
+        // set the meshes
+        GetComponent<MeshFilter>().sharedMesh = mesh;
+        GetComponent<MeshCollider>().sharedMesh = mesh;
     }
 
 }
