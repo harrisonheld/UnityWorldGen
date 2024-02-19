c0ecbb9c58319bf9253c2dd0e95e4ae7c3eefd40
Jake Bergin <mjberg111@gmail.com>
2024-02-19 10:40:12 -0600

    add slider to size and frequency

--- Editor/BiomeFeature.cs
+++ Editor/BiomeFeature.cs
@@ -11,10 +11,12 @@ public class BiomeFeature
 
     [SerializeField]
     [Tooltip("How often this feature will appear in the biome.")]
+    [Range(0, 50)]
     private int _frequency;
 
     [SerializeField]
     [Tooltip("The relative size of the features.")]
+    [Range(0.0f, 10.0f)]
     private float _size;
 
 }
