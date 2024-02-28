8b222ddf160195aaa3bdd0e42a4a518dec0aec95
Harrison Held <harrydheld@gmail.com>
2024-02-28 13:14:21 -0600

    Update NumericalTests.cs

--- Tests/Editor/NumericalTests.cs
+++ Tests/Editor/NumericalTests.cs
@@ -39,8 +39,12 @@ namespace UnitTests
         }
 
         [Test]
-        public void MultiHashDunderValueWorks()
+        public void MultiHashDummyValueWorks()
         {
+            // if you want to ensure that your results are entirely unique, you can throw a dummy value at the end
+            // for instance, we do a lot of MultiHash(x, y) when iterating over coordinates.
+            // we may want different results for each context, so you can throw the string "biomemap" or "simplexheightmap" at the end
+            // to ensure that the results are unique to that context. it doesn't really matter for now, but its a trick you can use.
             int hash1 = Helpers.MultiHash(1, 2, 3, "first");
             int hash2 = Helpers.MultiHash(1, 2, 3, "second");
             Assert.AreNotEqual(hash1, hash2);
